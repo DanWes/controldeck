431fa906b15d9574906646bd11933e515e8bfb4e
Daniel Weschke <daniel.weschke@directbox.de>
2021-04-03 23:41:09 +0200

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,8 +1,2 @@
 from setuptools import setup
-
-setup(
-    name='ControlDeck',
-    py_modules=['controldeck'],
-    entry_points={
-    'console_scripts': ['controldeck = controldeck:main', ],},
-)
+setup()
