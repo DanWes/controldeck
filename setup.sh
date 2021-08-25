7b205c761b3964900bc86ff76c897d6dd7e8ae50
Daniel Weschke <daniel.weschke@directbox.de>
2021-08-25 22:43:59 +0200

    rename systemd service file to indicate local setting and create a new file for a system installation

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ mkdir -p $HOME/.local/share/applications
 cp data/controldeck.desktop $HOME/.local/share/applications/controldeck.desktop
 sed -i "s|\${HOME}|${HOME}|" ~/.local/share/applications/controldeck.desktop
 mkdir -p $HOME/.config/systemd/user
-ln -sf $PWD/data/controldeck.service $HOME/.config/systemd/user/controldeck.service
+ln -sf $PWD/data/controldeck.service.local $HOME/.config/systemd/user/controldeck.service
 #cp data/controldeck.service $HOME/.config/systemd/user/controldeck.service
