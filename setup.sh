a21babbf8df38a118ef8e60a5c40a9fb74734849
Daniel Weschke <daniel.weschke@directbox.de>
2021-08-26 00:36:22 +0200

    rename .desktop file to indicate local setting and create a new file for a system installation

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pip install --user -e .
 
 mkdir -p $HOME/.local/share/applications
 #ln -sf $PWD/data/controldeck.desktop $HOME/.local/share/applications/controldeck.desktop
-cp data/controldeck.desktop $HOME/.local/share/applications/controldeck.desktop
+cp data/controldeck.desktop.local $HOME/.local/share/applications/controldeck.desktop
 sed -i "s|\${HOME}|${HOME}|" ~/.local/share/applications/controldeck.desktop
 mkdir -p $HOME/.config/systemd/user
 ln -sf $PWD/data/controldeck.service.local $HOME/.config/systemd/user/controldeck.service
